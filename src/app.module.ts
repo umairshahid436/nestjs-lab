403b5658cf7c97d384ffb47fcb1137e6aa48c400
umairshahid436 <umairshahid436@gmail.com>
2025-05-22 02:39:41 +0500

    proj structure using nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { MessagesModule } from './messages/messages.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
-  imports: [MessagesModule],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function bootStrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-bootStrap();
+bootstrap();
